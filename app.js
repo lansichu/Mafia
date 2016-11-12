c898fa2e99f6f84566297072b0100417d8313d95
huberpa <patrick.huber.gaggenau@freenet.de>
2016-11-11 17:55:11 -0800

    First save procedure

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 var express = require('express')
 var app = express()
+var db
+var MongoClient = require('mongodb').MongoClient
+
 
 app.get('/login', function (req, res) {
  
+	MongoClient.connect('mongodb://connection:connection@ds147777.mlab.com:47777/mafiadb', function (err, db) {
+  		if (err) throw err
 
- res.send("Try to save name: " + req.query.name);
+  		db.collection('Players').insert(
+		   {
+		     name: req.query.name,
+		     role: null,
+		     alive: true
+		   }
+		);
+
+res.send("Saved new Player "+req.query.name);
 
 
 
-// var MongoClient = require('mongodb').MongoClient
-// 	MongoClient.connect('mongodb://connection:connection@ds147777.mlab.com:47777/mafiadb', function (err, db) {
-//   		if (err) throw err
-//   		db.collection('Game').find({'_id':'MafiaGame'}).toArray(function (err, result) {
-//     			if (err) throw err
+  // 		db.collection('Game').find({'_id':'MafiaGame'}).toArray(function (err, result) {
+  //   			if (err) throw err
 
-//     			console.log(result)
-//   			res.send(result)
+  //   			console.log(result)
+  // 			res.send(result)
 
-//   })
-// })
+  // })
+})
 
 
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Master service activated...')
 })
 
 
