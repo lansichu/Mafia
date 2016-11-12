0b7182880f041443871021a028f800779eebb5d8
huberpa <patrick.huber.gaggenau@freenet.de>
2016-11-11 19:03:33 -0800

    Adopted return values for frontend

--- app.js
+++ app.js
@@ -9,17 +9,20 @@ app.get('/login', function (req, res) {
 	MongoClient.connect('mongodb://connection:connection@ds147777.mlab.com:47777/mafiadb', function (err, db) {
   		if (err) throw err
 
-  		db.collection('Players').insert(
-		   {
-		     name: req.query.name,
-		     role: null,
-		     alive: true
-		   }
-		);
+      if(req.query.name){
+    		db.collection('Players').insert(
+  		   {
+           _id: req.query.name,
+  		     name: req.query.name,
+  		     role: null,
+  		     alive: true
+  		   }
+  		  );
+      res.send("Success! Player added");
 
-res.send("Saved new Player "+req.query.name);
-
-
+      }else{
+        res.send("Error. Please try again");
+      }
 
   // 		db.collection('Game').find({'_id':'MafiaGame'}).toArray(function (err, result) {
   //   			if (err) throw err
