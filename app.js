c98f53dd28817d1a046ebebd71f6ef71c2464788
huberpa <patrick.huber.gaggenau@freenet.de>
2016-11-12 00:59:50 -0800

    Fixed duplicate id check. Closed Issue #2

--- app.js
+++ app.js
@@ -7,30 +7,33 @@ var MongoClient = require('mongodb').MongoClient
 app.get('/login', function (req, res) {
  
 	MongoClient.connect('mongodb://connection:connection@ds147777.mlab.com:47777/mafiadb', function (err, db) {
-  		if (err) throw err
+      if (err) throw err
+
 
       if(req.query.name){
-    		db.collection('Players').insert(
-  		   {
-           _id: req.query.name,
-  		     name: req.query.name,
-  		     role: null,
-  		     alive: true
-  		   }
-  		  );
-      res.send("Success! Player added");
+
+        db.collection('Players').find({_id: req.query.name}).toArray(function (err, result) {
+           if (err) throw err
+
+           if(result.length == 0){
+              db.collection('Players').insert(
+               {
+                 _id: req.query.name,
+                 name: req.query.name,
+                 role: null,
+                 alive: true
+               }
+              );
+            res.send("Success! Player added");
+            
+           }else{
+              res.send("Name already exists. Please choose another name");
+           }
+        })
 
       }else{
-        res.send("Error. Please try again");
+        res.send("Error. Something went wrong with the query parameters!");
       }
-
-  // 		db.collection('Game').find({'_id':'MafiaGame'}).toArray(function (err, result) {
-  //   			if (err) throw err
-
-  //   			console.log(result)
-  // 			res.send(result)
-
-  // })
 })
 
 
