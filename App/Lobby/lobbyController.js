88c6886e261190966dc55d347c168de7fe23b24c
LANSI CHU <chulansi@gmail.com>
2016-11-20 18:23:12 -0800

    Add 'Exit Game' button and redirect to login page

--- App/Lobby/lobbyController.js
+++ App/Lobby/lobbyController.js
@@ -2,6 +2,29 @@ angular.module("MafiaApp.lobby", [
     'ngMaterial',
     'ngRoute'
     ])
-.controller('lobbyController', function($scope) {
+.controller('lobbyController', function($scope, $mdDialog, $location) {
+    $scope.status = '  ';
+    $scope.customFullscreen = false;
             console.log("That's the lobby!");
-    })
+    $scope.showConfirm = function(ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+            .title('Would you like to exit the game?')
+            .textContent('Mafia has destroyed you.')
+            .ariaLabel('Decision Time')
+            .targetEvent(ev)
+            .ok('Get me out of here.')
+            .cancel('Keep me in!');
+
+
+        $mdDialog.show(confirm).then(function() {
+            $location.path('login');
+        }, function() {
+            console.log("stay");
+
+        });
+
+    };
+    });
+
+
--- App/mafiaController.js
+++ App/mafiaController.js
@@ -15,25 +15,6 @@ angular.module("MafiaApp", [
         .otherwise("/");
 })
 
-   .controller('loginController', function($scope, $http, $mdToast, $location, $rootScope) {
-       $scope.callBackend = function () {
-           console.log($scope.username);
-           $http.get('login?name=' + $scope.username).then(function (response) {
-               $scope.showStatus = response.data;
-               $mdToast.show(
-                   $mdToast.simple()
-                       .textContent($scope.showStatus.message)
-                       .hideDelay(3000)
-               )
-               if (response.data.success == true) {
-                   console.log()
-                   $location.path('lobby');
-               }
-           })
-       }
-   })
-
-
 .run(function($rootScope, $route, $location){
 
     $rootScope.$on('$locationChangeSuccess', function() {
